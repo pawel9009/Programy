a0bb9bf64a7a32a9d01b65275d841fbb9c789524
Pawel b <46341827+pawel9009@users.noreply.github.com>
2021-05-31 20:54:54 +0200

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,118 +1,185 @@
-WIN = 21
-lista =[]
-class TreeNode:
-    def __init__(self,value):
-        self.children=[]
-        self.value= value
-        self.parent=None
-        self.owner=0
-        self.depth=0
-        self.num=[1,2,3]
-        self.vin=0
+from tkinter import *
 
-    def add_child(self,child):
+lista = []
+score1= []
+
+def post_order(node):
+    if not node.children:
+        return node.value
+
+    if node.player1:
+        maxEva = -44444
+        score1 = []
+        for child in node.children:
+            eva = post_order(child)
+            maxEva = max(maxEva,eva)
+            score1.append(eva)
+            # print(maxEva, "max - glebokosc", child.depth, child.player1)
+        print(max(score1)," maxy")
+        return max(score1)
+
+    else:
+        minEva = 999999
+        score2 = []
+        for child in node.children:
+            eva = post_order(child)
+            minEva = min(minEva, eva)
+            score2.append(eva)
+            # print(minEva, "min -  glebokosc", child.depth, child.player1)
+        print(min(score2)," minima")
+        return min(score2)
+
+
+class Aplication(Frame):
+    WIN = 4
+    ZMIENNE = None
+
+    def __init__(self, master):
+        super(Aplication, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+        self.WIN = 0
+
+    def create_widgets(self):
+        Label(self,
+              text="Liczba n "
+              ).grid(row=0, column=0, sticky=W)
+        Label(self,
+              text="Dane po spacji "
+              ).grid(row=1, column=0, sticky=W)
+        self.win = Entry(self)
+        self.win.grid(row=0, column=1, sticky=W)
+
+        self.liczby = Entry(self)
+        self.liczby.grid(row=1, column=1, sticky=W)
+
+        Label(self,
+              ).grid(row=3, column=0, sticky=W)
+
+        Button(self,
+               text="Licz",
+               command=self.apka
+               ).grid(row=2, column=0, sticky=W)
+
+    def apka(self):
+        Aplication.WIN = int(self.win.get())
+        dane = self.liczby.get()
+        liczby = []
+        dane = list(dane.split(' '))
+        for x in dane:
+            liczby.append(int(x))
+        Aplication.ZMIENNE = liczby
+
+        root = build_product_tree()
+        root.print_tree()
+        wynik = post_order(root)
+        print(wynik, "wunik")
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.children = []
+        self.value = value
+        self.data = value
+        self.parent = None
+        self.depth = 0
+        self.player1 = True
+        self.end=False
+
+    def add_child(self, child):
         child.parent = self
-        child.value+=self.value
+        child.data = child.value
+        child.value += self.value
+
         self.children.append(child)
-        child.depth=self.depth+1
+        child.depth = self.depth + 1
+        if self.player1:
+            child.player1 = False
+        elif self.player1 == False:
+            child.player1 = True
         lista.append(child)
 
     def take_child(self):
         return self.children
 
-    def tak(self,dep):
-        if self.depth==dep:
+    def tak(self, dep):
+        if self.depth == dep:
             add(self)
-
 
     def get_level(self):
         level = 0
         p = self.parent
         while p:
-            level+=1
-            self.owner+=1
-            p=p.parent
+            level += 1
+            p = p.parent
         return level
 
-    def print_tree(self):
-        # print("antagonista"if self.owner%2==0 else"protagonista")
-        if(self.value==WIN):
-            print("Wygrana", "Protagonista"if self.get_level()%2==0 else "Antagonista")
-        elif(self.value>WIN):
-            print("przegrana")
+    # def post_order(self):
+    #
+    #     if self is not None:
+    #         for child in self.children:
+    #             self.droga += f" -> {child.value}"
+    #             child.post_order()
+    #
+    #         print(self.value, self.player1)
 
-        print("   "*self.depth, f"głębokość - {self.depth}, value {self.value} - ","P"if self.get_level()%2==0 else "A")
+    def print_tree(self):
+        if (self.value > Aplication.WIN):
+            if self.player1:
+                self.end=1
+            else:
+                self.end=-1
+            print("Przegrana", "Protagonista" if self.get_level() % 2 == 0 else "Antagonista")
+        elif (self.value == Aplication.WIN):
+            self.end=0
+            print("Remis")
+
+        print("   " * self.depth, f"głębokość - {self.depth}, value {self.value} - gracz ||{self.player1}")
         if self.children:
             for child in self.children:
                 child.print_tree()
 
-        # for child in root.children:
-        #     for x in range(len(child.num)):
-        #         child.add_child(TreeNode(child.num[x]))
-        # return root
 
-    # def rek(self,dept):
-    #     for node in self.children:
-    #         if self.depth > 5:
-    #             return
-    #         else:
-    #             node.tak(dept)
-    #             return self.rek(dept+1)
-# def rek(children):
-#     for x in children:
-#         print(x.value)
-#     for node in children:
-#         if node.value>WIN and node.vin ==0:
-#             node.vin=1
-#         elif node.value<WIN and node.vin==0:
-#             node.add_child(TreeNode(4))
-#             node.add_child(TreeNode(5))
-#             node.add_child(TreeNode(6))
-#             return add(node.children)
 def add(root):
-    for x in range(3):
-        root.add_child(TreeNode(root.num[x]))
+    for x in range(len(Aplication.ZMIENNE)):
+        root.add_child(TreeNode(Aplication.ZMIENNE[x]))
     return root
+
 
 def build_product_tree():
     root = TreeNode(0)
     root.tak(0)
-    tak=1
-    flaga=True
+    tak = 0
+    flaga = True
     while flaga:
         for x in lista:
             if x.depth == tak:
-                add(x)
+                if x.value < Aplication.WIN:
+                    add(x)
 
-        tak+=1
-        if tak>1:
-            flaga=False
-
-    print(len(lista))
-
-    # for node in new:
-    #     add(node)
-    #     cos = node.children
-    #     for x in cos:
-    #         x.tak()
-
-
-    # while x<7:
-    #     for child in new:
-    #         new = add(child)
-    #     new=new.children
-    #     x+=1
-
-
-    # for child in new:
-    #     new=add(child)
-    #     for c in new.children:
-    #         old = add(c)
-    #         for a in old.children:
-    #             old = add(a)
+        tak += 1
+        if tak > 18:
+            flaga = False
     return root
 
 
-root = build_product_tree()
-root.print_tree()
-print(len(lista))
+app = Tk()
+app.title("Aplikacja")
+app.geometry("300x80")
+app = Aplication(app)
+app.mainloop()
+
+# def minmax(self):
+#     if not self:
+#         return []
+#     stack = [self]
+#     last = None
+#     while stack:
+#         self = stack[-1]
+#         if not self.children or last and (last in self.children):
+#             print(self.value)
+#             stack.pop()
+#             last = self
+#         else:
+#             for child in self.children[::-1]:
+#                 stack.append(child)
